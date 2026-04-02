b2f05fce9ad9951dc0dd0410785d9aea91c4b022
Mafruhur Faruqi <mafruhurfaruqi@Mafruhurs-MacBook-Pro.local>
2026-04-02 12:04:31 +0600

    working on the website, not in the first cell, and that's okay

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/tools/startup-valuation-calculator/',
   test: {
     environment: 'node',
     globals: true,
